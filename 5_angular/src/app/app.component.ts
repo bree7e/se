527a464e25813d91a58d936a464db2b17254abe1
Alexandr Vetrov <vetrov@icc.ru>
2018-08-14 23:22:07 +0800

    Убрана императивная часть из компонента

--- 5_angular/src/app/app.component.ts
+++ 5_angular/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { PhotoService } from './photo.service';
 import Photo from './photo.model';
 import { PhotoServiceReactive } from './photo-reactive.service';
 import { Observable } from 'rxjs';
@@ -12,27 +11,15 @@ import { Observable } from 'rxjs';
     providers: [ PhotoServiceReactive ]
 })
 export class AppComponent implements OnInit {
-    photos: Photo[] = [];
     photos$: Observable<Photo[]>;
     showButton = true;
 
     constructor(
-        private photoService: PhotoService,
         private photoServiceReactive: PhotoServiceReactive
     ) {}
 
     ngOnInit() {
         this.photos$ = this.photoServiceReactive.photos$;
-    }
-
-    onLoadMore(): void {
-        const pageSize = 9;
-        this.photoService.getMorePhotos(pageSize).subscribe(photos => {
-            this.photos = [...this.photos, ...photos];
-            if (photos.length < pageSize) {
-                this.showButton = false;
-            }
-        });
     }
 
     onLoadMoreReactive(): void {
